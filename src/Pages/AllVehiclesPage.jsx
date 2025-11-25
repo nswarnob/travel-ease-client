5977f2bcf8c029c2f2b6fe792785f83d54b07689
Sharif Uddin Arnob <142154073+nswarnob@users.noreply.github.com>
2025-11-26 00:21:57 +0600

    minor fixing

--- src/Pages/AllVehiclesPage.jsx
+++ src/Pages/AllVehiclesPage.jsx
@@ -90,7 +90,7 @@ const AllVehiclesPage = () => {
     </div>
 
     {/* Filters */}
-    <div className="filter flex flex-wrap justify-center gap-2">
+    <div className="filter filter-reset flex flex-wrap justify-center gap-2">
       <input
         className="btn bg-base-300 rounded-full"
         onChange={handleSortChange}
